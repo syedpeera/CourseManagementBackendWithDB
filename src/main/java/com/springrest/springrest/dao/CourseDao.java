32a5decd2b798100e3b79b739ba5ef1de9b7fcfd
Syed Peera Saheb <peeraandroid.123@gmail.com>
2023-05-28 19:57:28 +0530

    Added @Respository Annotation

--- src/main/java/com/springrest/springrest/dao/CourseDao.java
+++ src/main/java/com/springrest/springrest/dao/CourseDao.java
@@ -1,9 +1,10 @@
 package com.springrest.springrest.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.springrest.springrest.entity.Course;
-
+@Repository
 public interface CourseDao extends JpaRepository<Course, Long>{
 
 }
